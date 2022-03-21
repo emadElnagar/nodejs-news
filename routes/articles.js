a26137f315fdb6e9fafe4231e1c8f9bca4a9be85
unknown <emad.m.alnagar@gmail.com>
2022-03-21 12:06:17 +0200

    create new article

--- routes/articles.js
+++ routes/articles.js
@@ -5,6 +5,41 @@ const async = require('hbs/lib/async');
 const slugify = require('slugify');
 const Category = require('../models/category');
 const Article = require('../models/article');
+const multer = require('multer');
+
+const fileFilter = function(req, file, cb) {
+  if (file.mimetype !== 'image/png') {
+    cb(null, true)
+  } else if (file.mimetype !== 'image/jpg') {
+    cb(null, true)
+  } else {
+    cb(new Error('please enter jpg or png image'), false)
+  }
+}
+
+const storage = multer.diskStorage({
+  destination:  function(req, file, cb) {
+    cb(null, './public/media/articles')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toDateString() + file.originalname)
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024*1024*5
+  },
+  fileFilter: fileFilter
+});
+
+router.use(upload.single('image'), (err, req, res, next) => {
+  if (err) {
+    req.flash('imageError', [err.message]);
+    res.redirect('new');
+  }
+});
 
 router.get('/', async(req, res, next) => {
   const articles = await Article.find({})
@@ -77,6 +112,7 @@ router.post('/create-category', [
 router.get('/new', async(req, res, next) => {
   let errorMessage = req.flash('error');
   const category = await Category.find({});
+  const imageErrorMessage = req.flash('profileError');
   if (req.session.user === undefined) {
     res.redirect('/');
   } else if (req.session.user.isAdmin === false) {
@@ -86,9 +122,58 @@ router.get('/new', async(req, res, next) => {
     title: 'Noticias-new-category',
     user: req.session.user,
     categories: category,
-    messages: errorMessage
+    messages: errorMessage,
+    imageErrorMessage: imageErrorMessage
   }
   res.render('articles/new_article', context);
+});
+
+router.post('/new', [
+  check('title')
+    .not().isEmpty().withMessage('please enter the article title'),
+  check('subject')
+    .not().isEmpty().withMessage('please enter the article subject'),
+  // check('image')
+  //   .not().isEmpty().withMessage('please upload the article image'),
+  check('category')
+    .not().isEmpty().withMessage('please choose the article category'),
+], (req, res, next) => {
+  const errors = validationResult(req);
+  if (! errors.isEmpty()) {
+    var validationErrors = [];
+    for(var i = 0; i < errors.errors.length; i++) {
+      validationErrors.push(errors.errors[i].msg);
+    }
+    req.flash('error', validationErrors);
+    res.redirect('new');
+    return;
+  }
+  const article = new Article({
+    title: req.body.title,
+    subject: req.body.subject,
+    image: (req.file.path).slice(6),
+    category: req.body.category,
+    author: req.session.user,
+    slug: slugify(req.body.title, {
+      replacement: '-',
+      lower: true,
+      strict: true,
+    })
+  });
+  Article.findOne({ title: req.body.title }, (err, result) => {
+    if (result) {
+      req.flash('error', 'this title is already exist try to change it');
+      res.redirect('new');
+      return;
+    } else {
+      article.save((err, data) => {
+        if (err) {
+          console.log(err);
+        }
+        res.redirect('/');
+      });
+    }
+  });
 });
 
 module.exports = router;
