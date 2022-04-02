000b38eff04d0fc6920a647869afc3d94c168013
unknown <emad.m.alnagar@gmail.com>
2022-04-02 14:57:32 +0200

    creating homepage and footer

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const article = require('../models/article');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Noticias', user: req.session.user });
+router.get('/', async(req, res) => {
+  const lastArticles = await article.find({}).sort('-updatedAt').limit(3);
+  const sportArticles = await article.find({ category: 'sport' }).sort('-updatedAt').limit(3);
+  const fashionArticles = await article.find({ category: 'fashion' }).sort('-updatedAt').limit(3);
+  res.render('index', {
+    title: 'Noticias',
+    user: req.session.user,
+    lastArticles: lastArticles,
+    sportArticles: sportArticles,
+    fashionArticles: fashionArticles
+  });
 });
 
 module.exports = router;
