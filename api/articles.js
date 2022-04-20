d00edcfe40299748bea34ec65838fd4318f596f6
unknown <emad.m.alnagar@gmail.com>
2022-04-20 22:55:32 +0200

    article details api

--- api/articles.js
+++ api/articles.js
@@ -5,12 +5,21 @@ const slugify = require('slugify');
 const Category = require('../models/category');
 const Article = require('../models/article');
 
-router.get('/', async(req, res, next) => {
+router.get('/', async(req, res) => {
   try {
     const articles = await Article.find({}).sort('-updatedAt');
     res.send(articles);
   } catch (err) {
     res.send(err);
+  }
+});
+
+router.get('/:slug', async(req, res) => {
+  const article = await Article.findOne({ slug: req.params.slug });
+  if(article) {
+    res.send(article);
+  } else {
+    res.status(404).send({message: 'Article not found'});
   }
 });
 
