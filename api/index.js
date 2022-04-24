f4113b172d7ad6cb2b41721cdb1fd624611004ff
unknown <emad.m.alnagar@gmail.com>
2022-04-24 23:23:42 +0200

    search api

--- api/index.js
+++ api/index.js
@@ -10,4 +10,20 @@ router.get('/', async(req, res) => {
   res.send({lastArticles, sportArticles, fashionArticles});
 });
 
+router.get('/search', async(req, res) => {
+  const { search } = req.query;
+  const searchedArticles = await article.find({ title: search });
+  const searchedArticlesCtg = await article.find({ category: search });
+  if (searchedArticles.length === 0 && searchedArticlesCtg.length === 0) {
+    res.status(404).json({
+      message: 'Not Found'
+    })
+  } else {
+    res.send({
+      searchedArticles,
+      searchedArticlesCtg
+    })
+  }
+})
+
 module.exports = router;
