bc5008a1ca084405151e0b06def791d0c6d36d9e
unknown <emad.m.alnagar@gmail.com>
2022-04-27 23:03:59 +0200

    upload profile image api

--- api/users.js
+++ api/users.js
@@ -2,7 +2,44 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
-const { isNotAuth } = require('../auth');
+const multer = require('multer');
+const fs = require('fs');
+const { isAuth, isNotAuth } = require('../auth');
+
+const fileFilter = function(req, file, cb) {
+  if (file.mimetype !== 'image/png') {
+    cb(null, true)
+  } else if (file.mimetype !== 'image/jpg') {
+    cb(null, true)
+  } else {
+    cb(new Error('please enter jpg or png image'), false)
+  }
+}
+
+const storage = multer.diskStorage({
+  destination:  function(req, file, cb) {
+    cb(null, './public/media/profile')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toDateString() + file.originalname)
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024*1024*5
+  },
+  fileFilter: fileFilter
+});
+
+router.use(upload.single('image'), (err, req, res, next) => {
+  if (err) {
+    res.status(400).json({
+      message: error.message
+    })
+  }
+});
 
 // USER REGISTER
 router.post('/register', isNotAuth, (req, res) => {
@@ -46,6 +83,34 @@ router.get('/profile/:id', async(req, res) => {
       message: 'User not found'
     })
   }
+});
+
+// UPLOAD PROFILE IMAGE
+router.post('/profile-img-upload', isAuth, async(req, res, next) => {
+  const user = req.session.user;
+  const newUser = { profileImg: (req.file.path).slice(6) };
+  const profile = await User.findById(req.session.user._id);
+  const path = './public' + profile.profileImg;
+  if (profile.profileImg !== '/images/default-user-image.png') {
+    fs.unlink(path).then(result => {
+      res.status(200).json({
+        message: 'Success'
+      })
+    }).catch(error => {
+      res.status(401).json({
+        message: 'Error' + error.message
+      })
+    })
+  }
+  User.updateOne({ _id: req.session.user._id }).then(result => {
+    res.status(200).json({
+      message: 'Image changed successfully'
+    })
+  }).catch(error => {
+    res.status(401).json({
+      message: 'Error' + error.message
+    });
+  });
 });
 
 module.exports = router;
