e4f6c00f0bc4477b18843484706f9c8f8d398437
unknown <emad.m.alnagar@gmail.com>
2022-04-30 00:31:00 +0200

    user signin api

--- api/users.js
+++ api/users.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const multer = require('multer');
 const fs = require('fs');
-const { isAuth, isNotAuth } = require('../auth');
+const { isAuth, isNotAuth, generateToken } = require('../authAPI');
 
 const fileFilter = function(req, file, cb) {
   if (file.mimetype !== 'image/png') {
@@ -41,6 +41,25 @@ router.use(upload.single('image'), (err, req, res, next) => {
   }
 });
 
+// USER LOGIN
+userRouter.post('/login', expressAsyncHandler(async(req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  if (user){
+    if (bcrypt.compare(req.body.password, user.password)) {
+      res.send({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
+      return;
+    }
+  }
+  res.status(401).send({message: 'invalid email or password'})
+}));
+
 // USER REGISTER
 router.post('/register', isNotAuth, (req, res) => {
   const user = new User({
@@ -54,7 +73,15 @@ router.post('/register', isNotAuth, (req, res) => {
     if (result) {
       res.status(402).json({
         message: 'user already created'
-      })
+      });
+      res.send({
+        _id: user._id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
     } else {
       user.save().then(result => {
         res.status(200).json({
@@ -87,7 +114,7 @@ router.get('/profile/:id', async(req, res) => {
 
 // UPLOAD PROFILE IMAGE
 router.post('/profile-img-upload', isAuth, async(req, res, next) => {
-  const user = req.session.user;
+  const user = await User.findById(req.user._id);
   const newUser = { profileImg: (req.file.path).slice(6) };
   const profile = await User.findById(req.session.user._id);
   const path = './public' + profile.profileImg;
@@ -114,8 +141,8 @@ router.post('/profile-img-upload', isAuth, async(req, res, next) => {
 });
 
 // EDIT USER NAME
-router.post('/edit-username', isAuth, (req, res) => {
-  const user = req.session.user;
+router.put('/edit-username', isAuth, (req, res) => {
+  const user = await User.findById(req.user._id);
   const newUser = { firstName: req.body.firstName, lastName: req.body.lastName };
   User.updateOne({ _id: req.session.user._id }, { $set: newUser }).then(result => {
     res.status(200).json({
@@ -129,8 +156,8 @@ router.post('/edit-username', isAuth, (req, res) => {
 });
 
 // CHANGE USER EMAIL
-router.post('/change-email', isAuth, (req, res) => {
-  const user = req.session.user;
+router.put('/change-email', isAuth, (req, res) => {
+  const user = await User.findById(req.user._id);
   const newUser = { email: req.body.email };
   User.updateOne({ _id: req.session.user._id }, { $set: newUser }).then(result => {
     res.status(200).json({
@@ -144,8 +171,8 @@ router.post('/change-email', isAuth, (req, res) => {
 });
 
 // CHANGE USER PASSWORD
-router.post('/change-password', isAuth, async(req, res) => {
-  const user = req.session.user;
+router.put('/change-password', isAuth, async(req, res) => {
+  const user = await User.findById(req.user._id);
   const profile = await User.findOne({ email: user.email });
   const validate = await bcrypt.compare(req.body.currentPassword, profile.password);
   if (!validate) {
---
+++ authAPI.js
@@ -0,0 +1,63 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (user) => {
+  return jwt.sign({
+    _id: user._id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    phoneNumber: user.phoneNumber,
+    gender: user.gender,
+    isAdmin: user.isAdmin,
+  }, process.env.JWT_SECRET , {
+    expiresIn: '30d',
+  });
+};
+
+const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET , (err, decode) => {
+      if(err) {
+        res.status(401).send({message: 'Invalid token'});
+      } else {
+        req.user = decode;
+        next();
+      }
+    })
+  } else {
+    res.status(401).send({message: 'No token'});
+  }
+}
+
+const isNotAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    const token = authorization.slice(7, authorization.length);
+    jwt.verify(token, process.env.JWT_SECRET , (err, decode) => {
+      if(err) {
+        res.status(401).send({message: 'Invalid token'});
+      } else {
+        req.user = decode;
+        next();
+      }
+    })
+  } else {
+    res.status(401).send({message: 'No token'});
+  }
+}
+
+const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: 'Invalid Admin Token' });
+  }
+};
+
+module.exports = {
+  isAdmin: isAdmin,
+  isNotAuth: isNotAuth,
+  isAuth: isAuth,
+}
